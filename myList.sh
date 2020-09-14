a1328d02e6fc26f71d77fecf3d8634b2454dd466
Matt Walter <mattwalter99@gmail.com>
2020-09-14 14:48:29 -0700

    colors and new line

--- myList.sh
+++ myList.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "Current directory: " $(basename "$PWD") > index.html
+echo -e "<h3> Current directory: /"$(basename "$PWD")"</h3>" > index.html
 
 for folder in $(ls -d */) 
 do 
-printf '%b\n' "$folder" >> index.html
+printf '<p style="color:0099FF;">%b\n' "/$folder""</p>" >> index.html
 done
 
 for file in $(ls -p | grep -v /)
 do 
-printf '%b\n' "$file" >> index.html
+printf '<p style="color:9900CC;">%b\n' "$file""</p>" >> index.html
 done
 xdg-open index.html
