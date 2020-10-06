6ee807c4d939e147fe5d3f806c0e3f91e7b1447d
Matt Walter <mattwalter99@gmail.com>
2020-10-06 12:28:48 -0700

    TestCases

--- testmap.py
+++ testmap.py
@@ -1,17 +1,16 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-driver.get("http://localhost:3000")
-assert "Wheelmap" in driver.title
-print(driver.title)
+class TestMap(unittest.TestCase):
 
-#elem = driver.find_element_by_name("q")
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+   
+   #INSERT EACH TEST CASE HERE 
 
-#elem.clear()
-#elem.send_keys("pycon")
-#elem.send_keys(Keys.RETURN)
+    def tearDown(self):
+        self.driver.close()
 
-#assert "No results found." not in driver.page_source
-
-#driver.close()
+if __name__ == "__main__":
+    unittest.main()
