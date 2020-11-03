9172d32a7d1c25cf96abe7cc1ef2f5b72a944b79
Matt Walter <mattwalter99@gmail.com>
2020-11-03 11:55:41 -0800

    assert fail

--- Parser.py
+++ Parser.py
@@ -5,13 +5,13 @@ import os
 
 def parse():
     testList = []
-    for path in glob('/TestAutomation/testCases/*.json'):  # loop over .json files in the cwd
+
+    for path in glob('./TestAutomation/testCases/*.json'):  # loop over .json files in the cwd
         with open(path) as f:
             data = json.load(f)  # opent the json file
             test = testCase(data['id'], data['requirement'], data['component'], data['input'], data['output'])
             testList.append(test)
     return testList
-
 
 class testCase:
 
--- testmap.py
+++ testmap.py
@@ -9,10 +9,12 @@ class TestMap(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
+        self.driver.get("http://localhost:3000")
 
     def test_function(input):
         def test(self):
             exec(input) in globals(), locals()
+            print("yoooo")
         return test
 
     def tearDown(self):
@@ -20,9 +22,11 @@ class TestMap(unittest.TestCase):
 
 
 if __name__ == "__main__":
+    
     testsmap = Parser.parse()
-    for test in testsmap.iteritems():
-        test_func = test(test.input)
+
+    for test in testsmap:
+        test_func = TestMap.test_function(test.input)
         setattr(TestMap, 'test_{0}'.format(test.id), test_func)
 
     unittest.main()
