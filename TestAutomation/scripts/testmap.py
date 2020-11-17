7f0acfdfaf605420e93291ddd6a11d12eae88eba
Matt Walter <mattwalter99@gmail.com>
2020-11-16 16:30:54 -0800

    jinja2 lameness

--- TestAutomation/scripts/testmap.py
+++ TestAutomation/scripts/testmap.py
@@ -7,10 +7,6 @@ import Parser
 
 
 class TestMap(unittest.TestCase):
-    req = 'req'
-    component = 'component'
-    input = 'input'
-    output = 'output'
         
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -28,16 +24,13 @@ class TestMap(unittest.TestCase):
 if __name__ == "__main__":
 
     testsmap = Parser.parse()
-    
 
     for test in testsmap:
         test_func = TestMap.test_function(test.input)
         setattr(TestMap, 'test_{0}'.format(test.id), test_func)
-        setattr(TestMap, 'req', test.req)
-
 
     template_args = {
-    "list_example": testsmap
+    "testCase_list": testsmap
     }
 
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(template='./scripts/template.html', template_args=template_args, output='../reports', report_name='testReport', open_in_browser=True, report_title='TestMap component test'))
