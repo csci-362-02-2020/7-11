ac28cdc276f103e8b7bba7b3a302f5455d5e5212
Janneke Morin <45515430+jannekemorin@users.noreply.github.com>
2020-12-04 01:39:33 -0500

    Update Parser.py

--- TestAutomation/scripts/Parser.py
+++ TestAutomation/scripts/Parser.py
@@ -1,19 +1,28 @@
+'''
+PARSER.PY
+
+This function parses all .json test case files in the testCases folder. The data for each file is stored as a testCase object. 
+The parse function returns a list of testCase objects to the driver. 
+'''
+
 from glob import glob
 import json
 import os
 
-
+# the parse function - loops over the .json test cases files, creating a list of test case objects
 def parse():
     testList = []
 
     for path in glob('./testCases/*.json'):  # loop over .json files in the cwd
         with open(path) as f:
-            data = json.load(f)  # opent the json file
+            data = json.load(f)  # open the .json file
             test = testCase(data['id'], data['requirement'], data['component'], data['input'], data['output'])
             testList.append(test)
     testList.sort(key=lambda x: x.id, reverse=False)
     return testList
 
+
+# the test case class - each test case object created by the parser is an instance
 class testCase:
 
     def __init__(self, id, req, component, input, output):
